d3d85a21add90941c630274f7eeeac4325518df4
Rodrigo Brentano <rodrigobjj24@gmail.com>
2024-03-22 00:13:25 -0300

    Removed logo icon in header

--- src/components/Header/Bottom/Bottom.js
+++ src/components/Header/Bottom/Bottom.js
@@ -1,17 +1,9 @@
 import React from "react";
-import LogoIcon from "../../../icons/LogoIcon.svg";
 import "./Styles.css";
 
 export default function Bottom() {
   return (
     <div className="headerBottom">
-      <div className="logoIconContainer">
-        <img
-          src={LogoIcon}
-          className="logoIcon"
-          alt="Logo Icon"
-        />
-      </div>
     </div>
   );
 };
